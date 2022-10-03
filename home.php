764a8e9981fa601caf7204a2f776e472b3e95211
manju <manju@gmail.com>
2022-10-03 17:57:11 +0530

    adding

---
+++ home.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/76746a0aa0.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="styel.css">
+</head>
+<body>
+  <section class="header">
+<a href="home.php" class="logo">travel.</a>
+
+<nav class="navbar">
+    <a href=home.php>home</a>
+    <a href=about.php>about</a>
+    <a href=package.php>package</a>
+    <a href=book.php>book</a>
+</nav>
+<div id="menu-btn" class="fas fa-bars"></div>
+  </section> 
+
+<!-- home section-->
+
+<section class="home">
+
+
+
+<div id="carouselExampleCaptions" class="carousel slide me" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active ">
+      <img src="images/home-slide-1.jpg" class="d-block w-100 " alt="...">
+      <div class="carousel-caption d-none d-md-block text-change">
+        <h1>explore,discover,travel</h1>
+        <p>travel around the world</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/home-slide-2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block text-change ">
+        <h1>explore,discover,travel</h1>
+        <p>discover new place.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/home-slide-3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block text-change">
+        <h1>explore,discover,travel</h1>
+        <p>make your tour worth</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+</section>
+
+
+
+
+<!--service section-->
+<section class="services">
+<h1 class="heading-title">our services</h1>
+
+<div class="box-container">
+    <div class="box">
+        <img src="images/icon-1.png" alt="miss">
+<h3>adventure</h3>
+    </div>
+    <div class="box">
+        <img src="images/icon-2.png" alt="miss">
+<h3>tour guid</h3>
+    </div>
+    <div class="box">
+        <img src="images/icon-3.png" alt="miss">
+<h3>trekking</h3>
+    </div>
+    <div class="box">
+        <img src="images/icon-4.png" alt="miss">
+<h3>camp fire</h3>
+    </div>
+    <div class="box">
+        <img src="images/icon-5.png" alt="miss">
+<h3>off road</h3>
+    </div>
+    <div class="box">
+        <img src="images/icon-5.png" alt="miss">
+<h3>camping</h3>
+    </div>
+</div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--footer -->
+  <section class="footer">
+<div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+    <a href=home.php> <i class="fas fa-angle-right"></i>
+        home</a>
+    <a href=about.php> <i class="fas fa-angle-right"></i>
+        about</a>
+    <a href=package.php> <i class="fas fa-angle-right"></i>
+        package</a>
+    <a href=book.php> <i class="fas fa-angle-right"></i>
+        book</a>  
+        </div>
+
+
+        <div class="box">
+            <h3>extra links</h3>
+    <a href="#"> <i class="fas fa-angle-right"></i>
+        ask questions</a>
+    <a href="#"> <i class="fas fa-angle-right"></i>
+        about us</a>
+    <a href="#"> <i class="fas fa-angle-right"></i>
+        privacy policy</a>
+    <a href="#"> <i class="fas fa-angle-right"></i>terms of use
+</a>
+    
+        </div>
+        <div class="box">
+            <h3>contact info</h3>
+    <a href="#"> <i class="fas fa-phone"></i>
+        +91 6754678998</a>
+    <a href="#"> <i class="fas fa-phone"></i>
+       080-76897865 </a>
+    <a href="#"> <i class="fas fa-envelope"></i>
+        karvy@gmail.com</a>
+    <a href="#"> <i class="fas fa-map"></i>
+       Bangalore, Karnataka,India</a>
+    
+    
+        </div>
+        <div class="box">
+            <h3>follow us</h3>
+    <a href="#"> <i class="fab fa-facebook-f"></i>
+       facebook</a>
+    <a href="#"> <i class="fab fa-twitter"></i>
+       twitter</a>
+    <a href="#"> <i class="fab fa-instagram"></i>
+       instagram</a>
+    
+    
+    
+        </div>
+
+</div>
+
+<div class="credit">
+    created by web developer manu
+</div>
+
+  </section>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="script.js">
+
+</script>
+</body>
+</html>
---
+++ package.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about page</title>
+
+
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/76746a0aa0.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="styel.css">
+</head>
+<body>
+  <section class="header">
+<a href="home.php" class="logo">travel.</a>
+
+<nav class="navbar">
+    <a href=home.php>home</a>
+    <a href=about.php>about</a>
+    <a href=package.php>package</a>
+    <a href=book.php>book</a>
+</nav>
+<div id="menu-btn" class="fas fa-bars"></div>
+  </section> 
+
+<!--package -->
+<div class="heading" style="background: url(images/header-bg-2.png) no-repeat;">
+    <h1>packages</h1>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--footer -->
+  <section class="footer">
+<div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+    <a href=home.php> <i class="fas fa-angle-right"></i>
+        home</a>
+    <a href=about.php> <i class="fas fa-angle-right"></i>
+        about</a>
+    <a href=package.php> <i class="fas fa-angle-right"></i>
+        package</a>
+    <a href=book.php> <i class="fas fa-angle-right"></i>
+        book</a>  
+        </div>
+
+
+        <div class="box">
+            <h3>extra links</h3>
+    <a href="#"> <i class="fas fa-angle-right"></i>
+        ask questions</a>
+    <a href="#"> <i class="fas fa-angle-right"></i>
+        about us</a>
+    <a href="#"> <i class="fas fa-angle-right"></i>
+        privacy policy</a>
+    <a href="#"> <i class="fas fa-angle-right"></i>terms of use
+</a>
+    
+        </div>
+        <div class="box">
+            <h3>contact info</h3>
+    <a href="#"> <i class="fas fa-phone"></i>
+        +91 6754678998</a>
+    <a href="#"> <i class="fas fa-phone"></i>
+       080-76897865 </a>
+    <a href="#"> <i class="fas fa-envelope"></i>
+        karvy@gmail.com</a>
+    <a href="#"> <i class="fas fa-map"></i>
+       Bangalore, Karnataka,India</a>
+    
+    
+        </div>
+        <div class="box">
+            <h3>follow us</h3>
+    <a href="#"> <i class="fab fa-facebook-f"></i>
+       facebook</a>
+    <a href="#"> <i class="fab fa-twitter"></i>
+       twitter</a>
+    <a href="#"> <i class="fab fa-instagram"></i>
+       instagram</a>
+    
+    
+    
+        </div>
+
+</div>
+
+<div class="credit">
+    created by web developer manu
+</div>
+
+  </section>
+<script src="script.js">
+
+</script>
+</body>
+</html>
